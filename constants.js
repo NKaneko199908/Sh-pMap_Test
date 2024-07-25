19e4f183349c36b30e52c4aff0b08126be7f50f7
NKaneko199908 <nk-kaneko@yachiyo-eng.co.jp>
2024-07-25 16:19:20 +0900

    スプレッドシート情報アップデート

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 const API_KEY = 'AIzaSyCq-4me5PTDrijLZHK1o3b53yRuJIxUyHE';
-const SHEET_ID = '1obyojZ6guLn1vG7u74rx0OIWGZds8BF0w58_FtpDksQ';
+const SHEET_ID = '1-PkYbrRJFvYjxGerXxoVSO678T3RGJ8HO7u4XhKHKjQ';
