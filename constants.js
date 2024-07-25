314d1c4407721ba9d44418fdac041cb5cded5622
NKaneko199908 <nk-kaneko@yachiyo-eng.co.jp>
2024-07-25 17:24:21 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 const API_KEY = 'AIzaSyCq-4me5PTDrijLZHK1o3b53yRuJIxUyHE';
-const SHEET_ID = '1-PkYbrRJFvYjxGerXxoVSO678T3RGJ8HO7u4XhKHKjQ';
+const SHEET_ID = '1DPA-TmQrqR3wMmM5j-dYKzLqObAgkXQ6hAnTm2aaMPA';
